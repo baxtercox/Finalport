598da4bcba210315e827a573ff396d33cc48e86c
Baxter Cox <baxtercoxrtr@gmail.com>
2021-02-03 19:40:08 -0500

    added a resume

---
+++ build/precache-manifest.1c3f14ddb37f3a54fffaf0fb611f3203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d66a02fc660e915d9ce0017fad67bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5846b81a71b148e90287",
+    "url": "/static/css/main.6d891a0e.chunk.css"
+  },
+  {
+    "revision": "0680ad4e0dfda2f3e9fc",
+    "url": "/static/js/2.fd327803.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fd327803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5846b81a71b148e90287",
+    "url": "/static/js/main.c9209f8b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,7 @@ import "../../App.css";
 export default function Resume() {
   return (
     <h1 className="sign-up">
-      <a href="https://drive.google.com/file/d/1rYb-GTXHAxUsihz4t-VMo0ODdMtB_8ob/view?usp=sharing">
+      <a href="https://docs.google.com/document/d/1C2JTnZ7RFoKMIv3WvsljNaT7l34v7bXeRJjn-5yKfzk/edit?usp=sharing">
         Resume
       </a>
     </h1>
